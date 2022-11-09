29f7dc95029a9c66bc11ee66ecf0459656b58862
georgejr11 <jljimenez1997@gmail.com>
2022-11-08 21:16:04 -0500

    prueba

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(title: 'Convertidor'),
+      home: const MyHomePage(title: 'Convertidor test'),
     );
   }
 }
